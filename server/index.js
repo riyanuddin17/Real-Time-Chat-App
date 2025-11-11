d1d03fa55f293d728160a7d5373fb816f453294c
Riyan Uddin <107648620+riyanuddin17@users.noreply.github.com>
2025-11-11 14:30:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3000;
 const ADMIN = 'Admin';
 
 const app = express();
@@ -163,3 +163,4 @@ function getUsersInRoom(room) {
 function getAllActiveRooms() {
   return Array.from(new Set(UsersState.users.map((user) => user.room)));
 }
+
