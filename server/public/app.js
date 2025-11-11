e76baa30b1bb20783f7d24eec239544491d5fd11
Riyan Uddin <107648620+riyanuddin17@users.noreply.github.com>
2025-11-11 14:38:51 +0530

    Update app.js

--- server/public/app.js
+++ server/public/app.js
@@ -1,4 +1,4 @@
-const socket = io('ws://localhost:3500');
+const socket = io('ws://localhost:3000');
 
 const msgInput = document.querySelector('#message');
 const nameInput = document.querySelector('#name');
@@ -109,3 +109,4 @@ function showRooms(rooms) {
     });
   }
 }
+
